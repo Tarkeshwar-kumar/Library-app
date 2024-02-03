7bf5dd20958ae8a2f46f30b3d4f6b1c59944946f
Tarkeshwar Kumar <ktarkeshwar305@gmail.com>
2024-02-03 09:53:50 +0530

    Added config.go

--- config/config.go
+++ config/config.go
@@ -1,12 +1,29 @@
 package config
 
+import (
+	"log"
 
-import ()
+	"github.com/spf13/viper"
+)
 
 var (
 	InitConfig = inticonfig
 )
 
-func inticonfig() [
-	
-]
+func inticonfig(filename string, additionalDirs []string) (err error) {
+	viper.SetConfigName(filename)
+	viper.AddConfigPath(".")
+
+	for _, dir := range additionalDirs {
+		viper.AddConfigPath(dir)
+	}
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		log.Println("Failed to read config file")
+	}
+
+	viper.ConfigFileUsed()
+	viper.WatchConfig()
+	return
+}
